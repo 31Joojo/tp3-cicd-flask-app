8c41bc6129e6cef0d54967d8c8322b34f9c61693
Joris LARMAILLARD <jorislarmaillard@gmail.com>
2025-11-19 12:04:02 +0100

    First commit

---
+++ tests/test_integration_example.py
@@ -0,0 +1,15 @@
+# tests/test_integration_example.py
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from src.app import app
+
+# Test health endpoint
+def test_health_endpoint():
+    client = app.test_client()
+
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.get_json() == {"status": "ok"}
---
+++ tests/test_unit_example.py
@@ -0,0 +1,17 @@
+import unittest
+from src.app import items
+
+class FirstUnitTest(unittest.TestCase):
+    def test_adding_item(self):
+        items.clear()
+
+        # Adding some items in the list of items
+        items.append('test_item1')
+        items.append('test_item2')
+        items.append('test_item3')
+
+        self.assertEqual(len(items), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
